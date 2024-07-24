4fc6a8f958dae4ddfed2c11ea424c0ee40a06238
Souvik962 <140205356+Souvik962@users.noreply.github.com>
2024-07-24 12:16:32 +0530

    Add files via upload
    this is portfolio website

---
+++ main6.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Student"," C and C++ Progreammer","python Devalover", "Matlab coder", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
